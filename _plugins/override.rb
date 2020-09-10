c83fc9e33457bcbfb31e1d8a3a42a64a51324bab
Ryan Hofschneider <ryan.hofschneider@gsa.gov>
2020-09-10 13:24:59 -0700

    Clean up code, add comments

--- _plugins/override.rb
+++ _plugins/override.rb
@@ -1,86 +1,133 @@
-require 'yaml'
+require "yaml"
 
 module Jekyll
-
   ##
-  # Loads 18F/uswds-jekyll YAML config files in ORGS_DIR and GUIDE_DIR, overriding any keys found in _data.
-  # Key precedence is GUIDE_DIR, ORGS_DIR, then _data.
-
+  # Loads custom, guide-specific config and data under GUIDE_DIR.
+  # Configuration keys are limited to those specified in this class,
+  # orginating from uswds-jekyll, jekyll-sitemap, and isidurs-bane.
+  # Data filenames/keys are limited to those in DATA_FILES.
+  #
   class OverrideGenerator < Generator
-    GUIDE_CONFIG_PATH = File.join("_guide", "_config.yml")
-    GUIDE_DATA_DIR = File.join("_guide", "_data")
+    # Guide-specific directory for configuration, data, and content
+    GUIDE_DIR = "_guide"
+    # Guide-specific configuration file
+    GUIDE_CONFIG = File.join("_guide", "_config.yml")
+
+    # Supported keys in GUIDE_CONFIG:
+
+    # Sets the guide title in the header and anchor, from uswds-jekyll
+    TITLE_KEY = "title"
+    # Sets the guide description in the <meta> element, from uswds-jekyll
+    DESC_KEY = "description"
+    # Sets the URL used in the sitemap.xml, from jekyll-sitemap
+    URL_KEY = "url"
+    # Sets the GitHub repository information for the "Edit this page" link, from uswds-jekyll
+    GITHUB_KEY = "github_info"
+    # Sets the search.gov handle used with the search function in the header, from uswds-jekyll
+    SEARCH_KEY = "search_site_handle"
+    # Sets the organization managing this guide, from isidurs-bane
+    ORG_KEY = "org"
+
+    # Parent directory for org-specific data
     ORGS_DATA_DIR = File.join("_data", "orgs")
 
-    TITLE_KEY = "title"
-    DESC_KEY = "description"
-    URL_KEY = "url"
-    GITHUB_KEY = "github_info"
-    SEARCH_HANDLE_KEY = "search_site_handle"
-    ORG_KEY = "org"
+    # Guide-specific data
+    GUIDE_DATA_DIR = File.join("_guide", "_data")
+
+    # Supported uswds-jekyll and isidurs-bane data files
+    DATA_FILES = ["header.yml", "navigation.yml", "footer.yml", "theme.yml", "anchor.yml"]
 
     safe true
 
+    ##
+    # Merges keys in GUIDE_CONFIG with those set in _config.yml.
+    # Also loads uswds-jekyll and isidurs-bane YAML data files under
+    # ORGS_DATA_DIR and in GUIDE_DATA_DIR, overriding any keys found
+    # in _data. Key precedence is GUIDE_DATA_DIR, then the optional
+    # org-specific sub-directory in ORGS_DATA_DIR, then finally _data.
+    #
+    # This method is run after Jekyll has made an inventory of
+    # the existing content, and before the site is generated, per:
+    # https://jekyllrb.com/docs/plugins/generators/
+    #
     def generate(site)
-      puts site.config[URL_KEY]
-      self.mergeConfig(site, GUIDE_CONFIG_PATH)
-      puts site.config[URL_KEY]
+      merge_config_file(site)
 
+      merge_org_data(site)
 
-      # Supported uswds-jekyll YAMLs (minus file extension)
-      yamls = ['header', 'navigation', 'footer', 'theme', 'anchor'] 
-
-      # Org-specific key values override those set in _data
-      yamls.each do |y|
-        if site.config['org']
-          self.mergeData(site, File.join(ORGS_DATA_DIR, site.config['org']), y)
-        end
-      end
-
-      # Guide-specific key values override anything preceding it
-      yamls.each do |y|
-        self.mergeData(site, GUIDE_DATA_DIR, y)
-      end
-
-      # TODO Address favicons.yml special case
-
-      puts site.config
+      merge_guide_data(site)
     end
 
-    def mergeConfig(site, path)
-      puts "        TODO: Merged " + path
+    private
 
-      required = ['title', 'description', 'github_info', 'search_site_handle']
+    def merge_org_data(site)
+      yamls = DATA_FILES
+      yamls.each do |y|
+        org = site.config[ORG_KEY]
+        if org
+          mergeData(site, File.join(ORGS_DATA_DIR, org), y)
+        end
+      end
+    end
+
+    def merge_guide_data(site)
+      yamls = DATA_FILES
+      yamls.each do |y|
+        mergeData(site, GUIDE_DATA_DIR, y)
+      end
+    end
+
+    ##
+    # Federalist does not allow multiple Jekyll config files to be
+    # specified (i.e., with --config), so this approximates that
+    # Jekyll feature.
+    #
+    def merge_config_file(site)
+      required = [TITLE_KEY, DESC_KEY, GITHUB_KEY, SEARCH_KEY, URL_KEY]
       optional = [ORG_KEY]
 
-      customizations = YAML.load_file(path)
+      original_url = site.config[URL_KEY]
+
+      customizations = YAML.load_file(GUIDE_CONFIG)
       if customizations
         required.each do |k|
           site.config[k] = customizations[k]
         end
-        
+
         optional.each do |k|
           if customizations.key?(k)
             site.config[k] = customizations[k]
           end
         end
 
-        if not site.config.key?(URL_KEY)
-          site.config[URL_KEY] = customizations[URL_KEY]
+        # Per https://jekyllrb.com/docs/variables/, Jekyll will set
+        # the url at runtime in dev environments -- preserve, if set
+        if original_url
+          site.config[URL_KEY] = original_url
         end
       end
+      puts "        Merged " + GUIDE_CONFIG
     end
 
-    def mergeData(site, dir, key)
+    def mergeData(site, dir, file)
+      path = File.join(dir, file)
+      unless File.exist?(path)
+        # Not every org/guide will need to override a given data file
+        return
+      end
+
       begin
-        path = File.join(dir, key + '.yml')
         customizations = YAML.load_file(path)
         if customizations
+          # Per https://jekyllrb.com/docs/datafiles/
+          key = File.basename(path, File.extname(path))
           site.data[key].merge!(customizations)
           puts "        Merged " + path
         end
       rescue
+        puts "         ERROR " + path
+        raise
       end
     end
   end
-
 end
